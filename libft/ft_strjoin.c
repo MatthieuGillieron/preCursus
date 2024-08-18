6de9ce748aedc57fed6cf96c23b6227837f9a9da
MatthieuGillieron <gillieron.matthieu@ik.me>
2024-08-18 11:48:21 +0200

    strtrim

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 15:57:43 by mg                #+#    #+#             */
-/*   Updated: 2024/08/16 15:58:20 by mg               ###   ########.fr       */
+/*   Updated: 2024/08/17 21:08:52 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,29 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	
+	char 	*dest;
+	int		i;
+	int		j;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (0);
+	dest = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!dest)
+		return (NULL);
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	j = i;
+	i = 0;
+	while (s2[i])
+	{
+		dest[j] = s2[i];
+		i++;
+		j++; 
+	}
+	dest[j] = '\0';
+	return (dest);
 }
